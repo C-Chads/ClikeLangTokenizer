da18c34afe005ca3066f6ee7a6810f5166020a87
gek169 <gek169>
2021-05-14 15:18:36 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -58,6 +58,19 @@ static void strll_show(strll* current, long lvl){
 		}
 	}
 }
+static void strll_recursive_parse_matched(strll* current_meta, char* l, char* r){
+	{
+		for(;current_meta != NULL && current_meta->text != NULL && current_meta->text[0] != '\"';
+			current_meta = current_meta->right)
+		{
+				parse_matched(current_meta, l, r);
+				if(current_meta->left)
+					strll_recursive_parse_matched(current_meta->left, l, r);
+				if(current_meta->child)
+					strll_recursive_parse_matched(current_meta->child, l, r);
+		}
+	}
+}
 
 int main(int argc, char** argv){
 	char* larg;  long i;
@@ -86,8 +99,10 @@ int main(int argc, char** argv){
 		exit(1);
 	}
 	entire_input_file = read_file_into_alloced_buffer(ifile, &entire_input_file_len);
+	/*if(entire_input_file_len > 0) entire_input_file[entire_input_file_len-1] = '\0';*/
 	fclose(ifile); ifile = NULL;
 	/*Find all instances of illegal characters and replace them with spaces.*/
+	/*
 	{long i = 0; long len = strlen(entire_input_file);
 		for(i = 0; i < len; i++)
 			{
@@ -101,13 +116,6 @@ int main(int argc, char** argv){
 					}
 			}
 	}
-	/*Replace all double spaces.*/
-	/*
-	local_search_point = strfind(entire_input_file, "  ");
-	while(local_search_point > -1){
-		entire_input_file = str_repl_allocf(entire_input_file, "  ", " ");
-		local_search_point = strfind(entire_input_file, "  ");
-	}
 	*/
 	/*Force widening of double parentheses groups.*/
 	
@@ -116,40 +124,111 @@ int main(int argc, char** argv){
 	printf("\n\n");
 	/*tokenized = parse_matched(entire_input_file, "{", "}");*/
 	
-	tokenized = tokenize(entire_input_file, ";");
-	/*
-	{strll* current = &tokenized;
-		for(;current != NULL; current = current->right){
-			if(current->text)
-				printf("TOKEN IS:%s\n", current->text);
-			if(current->child)
-				printf("CHILD IS:%s\n", current->child->text);
-		}
-	}*/
-	/**/
-
+	tokenized = tokenize(entire_input_file, "\n");
+	puts("~~~~~AFTER TOKENIZATION~~~~~");
+	strll_show(&tokenized, 0);
+	puts("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	{strll* current_meta = &tokenized;
+			for(;current_meta != NULL && current_meta->text != NULL; current_meta = current_meta->right){
+					while(
+					current_meta->text && 
+							(
+								isspace(current_meta->text[0])
+							)
+					)
+					{
+						char* text_old = current_meta->text;
+						char* text_new = strcatalloc(text_old + 1, "");
+						free(text_old);
+						current_meta->text = text_new;
+					}
+			}
+	}
 	{strll* current_meta = &tokenized;
 		for(;current_meta != NULL && current_meta->text != NULL; current_meta = current_meta->right)
 		{
+	
 			strll* current = current_meta;
 			strll* retval = current_meta;
-			do{ 
+			do{
 				current = retval;
 				current_meta = retval;
-				retval = consume_until(current, "|", 0);
-				if(retval != current)
-					parent_right_node(current);
+				if(current->text){
+					 long loc_next_semicolon;
+					long loc_firstquote = strfind(current->text, "\"");
+					loc_next_semicolon = strfind(current->text, ";");
+					/*FOUND SOME QUOTES*/
+					if(
+						loc_firstquote > -1 &&
+						!(loc_next_semicolon > -1 && loc_next_semicolon < loc_firstquote)
+					)
+					{ long ii;
+						/*There is indeed a set of quotes!*/
+						if(loc_firstquote > 0){
+							retval = consume_bytes(current, loc_firstquote);
+							current = retval;
+							current_meta = retval;
+						} else {
+							/*printf("\nCurrent seems to have a quote at the beginning...%s\n", current->text);*/
+						}
+						for(ii = 1; current->text[ii]!= '\0'; ii++){
+							if(current->text[ii] == '\\' && current->text[ii] != '\0'){
+								ii++;continue; /*Skip the escaped character.*/
+							} else if(current->text[ii] == '\\' && current->text[ii] == '\0'){
+								puts("ERROR: unmatched quote group.");
+								exit(1);
+							}
+							if(current->text[ii] == '\"'){
+								break;
+							}
+						}
+						if(current->text[ii] != '\"'){
+							puts("ERROR: unmatched quote group.");
+							exit(1);
+						}
+						retval = consume_bytes(current, ii+1); 
+						/*Consume the second quote too.*/
+						current = retval;
+						current_meta = retval;
+					} else {
+						retval = consume_until(current, ";", 0);
+					}
+				} else {
+					puts("Internal Error.");
+					exit(1);
+				}
 			}while(retval != current);
 		}
 	}
-
 	{strll* current_meta = &tokenized;
-		for(;current_meta != NULL && current_meta->text != NULL; current_meta = current_meta->right)
-		{
-				parse_matched(current_meta, "{", "}");
-		}
+			for(;current_meta != NULL && current_meta->text != NULL; current_meta = current_meta->right){
+					while(
+					current_meta->text && 
+							(
+								isspace(current_meta->text[0])
+							)
+					)
+					{
+						char* text_old = current_meta->text;
+						char* text_new = strcatalloc(text_old + 1, "");
+						free(text_old);
+						current_meta->text = text_new;
+					}
+					while(
+					current_meta->text && 
+							(	strlen(current_meta->text) > 0 &&
+								isspace(current_meta->text[strlen(current_meta->text)-1])
+							)
+					)
+					{
+						char* text_old = current_meta->text;
+						char* text_new = str_null_terminated_alloc(text_old, strlen(text_old) - 1);
+						free(text_old);
+						current_meta->text = text_new;
+					}
+			}
 	}
+	strll_recursive_parse_matched(&tokenized, "{", "}");
 	strll_show(&tokenized, 0);
-
 	return 0;
 }
